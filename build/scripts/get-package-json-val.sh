b55288d59e28db3cfc57e73b0157d1fe21593c68
Christian Blos <christian.blos@gmx.de>
2018-06-05 09:59:47 +0200

    updated script to create zip files

--- build/scripts/get-package-json-val.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo $(cat package.json \
-  | grep $1 \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/ *"//' \
-  | sed 's/", *//')
--- build/scripts/zip-files.sh
+++ build/scripts/zip-files.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+function getPackageJsonVal {
+    echo $(cat package.json \
+      | grep $1 \
+      | head -1 \
+      | awk -F: '{ print $2 }' \
+      | sed 's/ *"//' \
+      | sed 's/", *//')
+}
+
 DIR="$(dirname "$0")"
-PACKAGE_VERSION=$(${DIR}/get-package-json-val.sh version)
-PRODUCT_NAME=$(${DIR}/get-package-json-val.sh productName)
+PACKAGE_VERSION=$(getPackageJsonVal version)
+PRODUCT_NAME=$(getPackageJsonVal productName)
 
 echo "Zip files..."
 mkdir dist/upload
